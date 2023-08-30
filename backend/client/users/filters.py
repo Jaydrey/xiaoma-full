835d1f39ac83484a69a0bf608aeb404c1bf1fbe1
Jaydrey <jaribwetshi64@gmail.com>
2023-08-30 16:27:34 +0300

    feat(filters): create filters for rider, and gender

---
+++ backend/client/users/filters.py
@@ -0,0 +1,46 @@
+import django_filters
+from django_filters import FilterSet
+
+from .models import User, Gender
+
+
+class RidersFilter(FilterSet):
+    first_name = django_filters.CharFilter(lookup_expr="icontains")
+    last_name = django_filters.CharFilter(lookup_expr="icontains")
+    username = django_filters.CharFilter(lookup_expr="icontains")
+    is_active = django_filters.BooleanFilter()
+
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "is_active",
+        )
+
+
+class RiderFilter(FilterSet):
+    id = django_filters.CharFilter(lookup_expr="exact")
+
+    class Meta:
+        model = User
+        fields = ("id",)
+
+
+class GenderFilter(FilterSet):
+    id = django_filters.CharFilter(lookup_expr="exact")
+    type = django_filters.CharFilter(lookup_expr="icontains")
+
+    class Meta:
+        model = Gender
+        fields = ("id", "type")
+
+
+# class StatusFilter(FilterSet):
+#     id = django_filters.CharFilter(lookup_expr="exact")
+#     type = django_filters.CharFilter(lookup_expr="icontains")
+
+#     class Meta:
+#         model = Status
+#         fields = ("id", "type")
