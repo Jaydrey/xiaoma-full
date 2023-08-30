fe13a04126d1ca0472854c2c98dbb625fcb92235
Jaydrey <jaribwetshi64@gmail.com>
2023-08-30 15:37:22 +0300

    feat(types): add trips, trip status and cancelation reason type

---
+++ backend/client/trips/types.py
@@ -0,0 +1,81 @@
+from graphene import (
+    relay,
+    UUID,
+    String,
+    Float,
+    Field,
+    JSONString,
+    ObjectType
+)
+from graphene_django import DjangoObjectType
+
+import graphql_geojson
+
+
+# models
+from .models import (
+    Trip,
+    TripStatus,
+    CancellationReason,
+)
+
+
+class TripsType(DjangoObjectType):
+    class Meta:
+        model = Trip
+        exclude_fields = (
+            "rider",
+            "driver",
+            "pickup_time",
+            "distance",
+            "pickup_location",
+            "dropoff_location",
+            "cancellation_reason",
+            "created_at",
+            "updated_at",
+        )
+        interfaces = (relay.Node, )
+    pickup_location_coords = JSONString()
+    dropoff_location_coords = JSONString()
+
+    def resolve_pickup_location_coords(self, info):
+        return self.pickup_location_coords
+
+    def resolve_dropoff_location_coords(self, info):
+        return self.dropoff_location_coords
+
+
+class TripType(DjangoObjectType):
+    class Meta:
+        model = Trip
+        exclude_fields = (
+            "pickup_location",
+            "dropoff_location",
+            "updated_at",
+        )
+        interfaces = (relay.Node,)
+    pickup_location_coords = JSONString()
+    dropoff_location_coords = JSONString()
+    trip_id = UUID(source="id")
+
+    def resolve_pickup_location_coords(self, info):
+        return self.pickup_location_coords
+
+    def resolve_dropoff_location_coords(self, info):
+        return self.dropoff_location_coords
+
+
+class TripStatusType(DjangoObjectType):
+    class Meta:
+        model = TripStatus
+        fields = ("id", "type")
+        interfaces = (relay.Node,)
+    trip_status_id = UUID(source="id")
+
+
+class CancellationReasonType(DjangoObjectType):
+    class Meta:
+        model = CancellationReason
+        fields = ("id", "reason", "created_at")
+        interfaces = (relay.Node,)
+    cancellation_reason_id = UUID(source="id")
