e2f83e8b51b292371654405ebc5b5a75f250ea94
Jaydrey <jaribwetshi64@gmail.com>
2023-08-31 10:53:32 +0300

    feat(chat): create screens and stores for chat screen

---
+++ mobile/xiaoma/lib/chat/screens/chat_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+  static const routeName = "chat-screen";
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Center(
+          child: Text("Chats"),
+        ),
+      ),
+    );
+  }
+}
