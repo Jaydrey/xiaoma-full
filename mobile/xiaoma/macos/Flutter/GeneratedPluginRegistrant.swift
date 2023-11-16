40a48c4c6991634ccee2477d1cbef30f4898d0c3
Jaydrey <jaribwetshi64@gmail.com>
2023-11-16 12:28:20 +0300

    build(mobile): build the xiaoma application

--- mobile/xiaoma/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/xiaoma/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,18 +5,24 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_macos
+import connectivity_plus
 import geolocator_apple
 import location
 import package_info
 import package_info_plus
+import path_provider_foundation
 import shared_preferences_foundation
 import smart_auth
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
 }
