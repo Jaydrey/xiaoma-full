43b8e74b994d0277af9c96b65d9557d1c41ceab2
Jaydrey <jaribwetshi64@gmail.com>
2023-11-16 12:22:30 +0300

    feat(views): create user and gender view endpoints

--- backend/client/users/views.py
+++ backend/client/users/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+
+from rest_framework.permissions import AllowAny
+
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+# views
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+# models
+from .models import (
+    Gender,
+    User
+)
+
+# serializers
+from .serializers import (
+    UserSerializer,
+    GenderSerializer,
+    CreateGenderSerializer,
+)
+
+# swagger
+from drf_spectacular.utils import (
+    extend_schema,
+)
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @extend_schema(exclude=True)
+    def create(self, request, *args, **kwargs):
+        return None
+        
+class GenderViewSet(ModelViewSet):
+    queryset = Gender.objects.all()
+    serializer_class = GenderSerializer
