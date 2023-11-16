fea14c0df89e62372b6924e1da6207631cdddef4
Jaydrey <jaribwetshi64@gmail.com>
2023-11-16 12:12:04 +0300

    feat(views): create trip, status and cancellation endpoint views

--- backend/client/trips/views.py
+++ backend/client/trips/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+
+from rest_framework.permissions import AllowAny
+
+# serializers
+from .serializers import (
+    TripSerializer,
+    CancellationReasonSerializer,
+    TripStatusSerializer,
+)
+
+# models
+from .models import (
+    CancellationReason,
+    Trip,
+    TripStatus,
+)
+
+
+class TripViewSet(ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+class CancellationReasonViewSet(ModelViewSet):
+    queryset = CancellationReason.objects.all()
+    serializer_class = CancellationReasonSerializer
+
+
+class TripStatusViewSet(ModelViewSet):
+    queryset = TripStatus.objects.all()
+    serializer_class = TripStatusSerializer
+
+    
